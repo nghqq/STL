82c4e5c40f510a24803c1d34e9efd61d0992420e
- <-@DESKTOP-5LUH4K3>
2023-11-06 09:46:56 +0300

    Police Done

--- Police/main.cpp
+++ Police/main.cpp
@@ -1,10 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include<Windows.h>
 #include<iostream>
 #include<fstream>
 #include<string>
 #include<list>
 #include<map>
 #include<ctime>
+#include<conio.h>
 
 #define tab "\t"
 #define delimiter "\n-------------------------------------\n"
@@ -107,9 +109,26 @@ public:
 		set_place(place);
 		set_time(time.c_str());
 	}
+	Crime(int id, const std::string& place, std::time_t time)
+	{
+		set_id(id);
+		set_place(place);
+		set_time(time);
+	}
 	~Crime(){}
 	
 };
+/*std::istream& operator >>(std::istream& is, Crime& obj)
+	{
+	char sz_buffer[_MAX_PATH]{};
+	is.getline(sz_buffer, _MAX_PATH);
+	obj.set_time(atoi(sz_buffer));
+	obj.set_id(atoi(strrchr(sz_buffer, ' ') + 1));
+	*strrchr(sz_buffer, ' ') = 0;
+	char* psz_place = strchr(sz_buffer, ' ') + 1;
+	obj.set_place(psz_place);
+	return is;
+	}*/
 	std::ostream& operator<<(std::ostream& os, const Crime& obj) 
 	{
 		const int SIZE = 32;
@@ -175,54 +194,121 @@ public:
 	std::ifstream& getline(std::ifstream& ifs, LicencePlate& obj, const char delim) 
 	{
 		std::string plate;
-		std::getline(ifs,plate, delim);
-		obj.set_plate(plate);
-		return ifs;
+std::getline(ifs, plate, delim);
+obj.set_plate(plate);
+return ifs;
 	}
-	
+
 
 
 	void print(const std::map<LicencePlate, std::list<Crime>>& base);
-	void print(const std::map<LicencePlate, std::list<Crime>>& base,LicencePlate plate);
-	void print(const std::map<LicencePlate, std::list<Crime>>& base,LicencePlate start_plate,LicencePlate end_plate);
-	void save(const std::map<LicencePlate, std::list<Crime>>& base,const std:: string& filename);
+	void print(const std::map<LicencePlate, std::list<Crime>>& base, LicencePlate plate);
+	void print(const std::map<LicencePlate, std::list<Crime>>& base, LicencePlate start_plate, LicencePlate end_plate);
+	void save(const std::map<LicencePlate, std::list<Crime>>& base, const std::string& filename);
 	std::map<LicencePlate, std::list<Crime>>load(const std::string& filename);
-	
-	
 
 
-void main() 
-{
-	setlocale(LC_ALL, "");
-	//Crime crime;
-	//crime.set_time("16:20 24/10/2023");
-	//std::cout << delimiter << std::endl;
-	//crime.set_time("2023/10/24 4:20");
 
-	//Crime crime(1, "ул.Ленина", "16:20 24/10/2023");
-	//std::cout << crime << std::endl;
 
-	//LicencePlate plate("m777ab"); std::cout << plate << std::endl;
-	//LicencePlate plate_1("VasyaTupenko"); std::cout << plate_1 << std::endl;
-
-	/*std::map < LicencePlate, std::list<Crime>> base
+	void main()
 	{
-		{LicencePlate("m777ab"),{Crime(1,"ул.Ленина","12:20 18.05.2023"),Crime(3,"ул.Ленина","12:20 18.05.2023")}},
-		{LicencePlate("m001bb"),{Crime(4,"ул.Космонавтов","22:20 20.10.2023"),Crime(5,"ул.Космонавтов","22:30 18.05.2023"),Crime(6,"ул.Космонавтов","22:30 18.05.2023")}},
-		{LicencePlate("a358nt"),{Crime(2,"ул.Пушкина","14:25 22.10.2023"),Crime(9,"ул.Пушкина","14:00 22.10.2023")}},
-	};
-	print(base);
-	save(base, "base.txt");*/
-	std::map<LicencePlate, std::list<Crime>> base = load("base.txt");
-	print(base);
+		setlocale(LC_ALL, "");
+		//Crime crime;
+		//crime.set_time("16:20 24/10/2023");
+		//std::cout << delimiter << std::endl;
+		//crime.set_time("2023/10/24 4:20");
 
-	LicencePlate plate;
-	std::cout << "Введите номер: "; std::cin >> plate;
-	print(base, plate);
-	system("PAUSE");
-	system("CLS");
-	main();
+		//Crime crime(1, "ул.Ленина", "16:20 24/10/2023");
+		//std::cout << crime << std::endl;
 
+		//LicencePlate plate("m777ab"); std::cout << plate << std::endl;
+		//LicencePlate plate_1("VasyaTupenko"); std::cout << plate_1 << std::endl;
+
+		/*std::map < LicencePlate, std::list<Crime>> base
+		{
+			{LicencePlate("m777ab"),{Crime(1,"ул.Ленина","12:20 18.05.2023"),Crime(3,"ул.Ленина","12:20 18.05.2023")}},
+			{LicencePlate("m001bb"),{Crime(4,"ул.Космонавтов","22:20 20.10.2023"),Crime(5,"ул.Космонавтов","22:30 18.05.2023"),Crime(6,"ул.Космонавтов","22:30 18.05.2023")}},
+			{LicencePlate("a358nt"),{Crime(2,"ул.Пушкина","14:25 22.10.2023"),Crime(9,"ул.Пушкина","14:00 22.10.2023")}},
+		};
+		print(base);
+		save(base, "base.txt");*/
+		std::map<LicencePlate, std::list<Crime>> base = load("base.txt");
+		print(base);
+
+		/*LicencePlate plate;
+		std::cout << "Введите номер: "; std::cin >> plate;
+		print(base, plate);*/
+		/*LicencePlate start_plate, end_plate;
+		std::cout << "Введите начальный номер: "; std::cin >> start_plate;
+		std::cout << "Введите конечный номер номер: "; std::cin >> end_plate;
+		print(base, start_plate, end_plate);*/
+		//system("PAUSE");
+		//system("CLS");
+		//main();
+
+		char key;
+		do
+		{
+			system("CLS");
+			std::cout << "1.Вывод всей базы ;" << std::endl;
+			std::cout << "2.Вывод информации по номеру ;" << std::endl;
+			std::cout << "3.Вывод информации по диапазону номеров ;" << std::endl;
+			std::cout << "4.Добавить нарушение ;" << std::endl;
+			std::cout << "5.Сохранить базу в файл ;" << std::endl;
+			std::cout << "6.Загрузить базу из файла ;" << std::endl;
+			key = _getch();
+			switch (key)
+			{
+			case '1': print(base); break;
+			case '2':
+			{
+				LicencePlate plate;
+				std::cout << "Введите номер: "; std::cin >> plate;
+				print(base, plate);
+				break;
+			}
+			case '3':
+			{
+				LicencePlate start_plate;
+				LicencePlate end_plate;
+				std::cout << "Введите начальный номер: "; std::cin >> start_plate;
+				std::cout << "Введите конечный номер: "; std::cin >> end_plate;
+				print(base, start_plate, end_plate);
+			}
+				break;
+			case '4':
+			{
+				LicencePlate plate;
+				std::string place;
+				int id;
+				std::cout << "Введите номер: "; std::cin >> plate;
+				std::cout << "Введите место: ";
+				std::cin.clear();
+				std::cin.ignore();
+				SetConsoleCP(1251);
+				std::getline(std::cin, place);
+				SetConsoleCP(866);
+				std::cout << "Выберите совершенное нарушение:\n";
+				for (std::pair<int, std::string>i : VIOLATIONS)std::cout << i.first << ". " << i.second << std::endl;
+				std::cin >> id;
+				Crime crime(id, place, time(NULL));
+				base[plate].push_back(crime);
+				std::cout << plate << ":\n";
+				for (Crime i : base[plate])std::cout << i << ";\n";
+				
+			}
+			case'5':
+				save(base, "base.txt"); break;
+
+			case'6':
+
+				std::map<LicencePlate, std::list<Crime>> base = load("base.txt");
+				print(base); break;
+			}
+
+			system("PAUSE");
+			
+		} while (key != 27);
  }
 
 void print(const std::map<LicencePlate, std::list<Crime>>& base) 
@@ -256,7 +342,15 @@ void print(const std::map<LicencePlate, std::list<Crime>>& base, LicencePlate pl
 }
 void print(const std::map<LicencePlate, std::list<Crime>>& base, LicencePlate start_plate, LicencePlate end_plate) 
 {
-
+	for (std::map<LicencePlate, std::list<Crime>>::const_iterator bit = base.lower_bound(start_plate); bit != base.upper_bound(end_plate); ++bit) 
+	{
+		std::cout << bit->first << ":\n";
+		for (std::list<Crime>::const_iterator it = bit->second.begin(); it != bit->second.end(); ++it) 
+		{
+			std::cout << tab << *it << ":\n";
+		}
+		std::cout << std::endl;
+	}
 }
 
 void save(const std::map<LicencePlate, std::list<Crime>>& base, const std::string& filename) 
